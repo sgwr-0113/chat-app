ad6d90c6e980830126a7bc4875c2b6a5e4ca584c
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-07-02 22:12:14 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create, :destroy]
 end
